3966b5f2dd9896661794287bb6018b2b3de58506
Stanislav Efimtsev <evendot@yandex.ru>
2025-11-13 20:31:48 +0300

    [Database] Added db connection function

---
+++ storage/database/postgres.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+type Config struct {
+	Host		string
+	Port		string
+	Password	string
+	User		string
+	DBName		string
+	SSLMode		string
+}
+
+func NewConnection(config *Config)(*gorm.DB, error){
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
+	if err != nil {
+		log.Fatal("An error occured while establishing a connection with the database!")
+		return db, err
+	}
+	return db, nil
+}
